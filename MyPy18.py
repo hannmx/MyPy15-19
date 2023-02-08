ddc00e45014faaf6b72c641cdc7f5e4b4d1afada
hannmx <mxkud@mail.ru>
2023-02-08 15:54:50 +0300

    Программа, которая выводит в файл многочлен степени k, которую вводит пользователь

--- MyPy18.py
+++ MyPy18.py
@@ -6,19 +6,29 @@ k = int(input('Введите натуральную степень многоч
 #заполнение списка коэффициентов многочлена в зависимости от степени
 coeff_list = []     
 for i in range(k + 1):
-     coeff_list.append(random.randint(0,5))
+     coeff_list.append(random.randint(0,3))
 print(coeff_list)
 
-f = open('18Python.txt', 'a')
+f = open('18Python.txt', 'w')
+f.write(f'A polynomial of {k} degrees: ')
 while k > 0:
     for i in range(len(coeff_list)):
         if coeff_list[i] == 0:
-            print(end='')
-            k -= 1
+            print(end='', file=f)
         elif k == 0:
-            print(str(coeff_list[i]), end= '')
+            print(str(coeff_list[i]), end= '', file=f)
+        elif k == 1 and coeff_list[i + 1] == 0:
+            print(str(coeff_list[i]) + 'x', end='', file=f)
+        elif k == 1:
+            if coeff_list[i] == 1:
+                print('x + ', end='', file=f)
+            else:
+                print(str(coeff_list[i]) + 'x + ', end='', file=f)
         else:
-            print(str(coeff_list[i]) + 'x^' + str(k) + ' + ', end='') 
-            k -= 1
-print(' = 0')
-f.close()
+            if coeff_list[i] == 1:
+                print('x^' + str(k) + ' + ', end='', file=f)
+            else:
+                print(str(coeff_list[i]) + 'x^' + str(k) + ' + ', end='', file=f) 
+        k -= 1
+print(' = 0', file=f)
+f.close()  
